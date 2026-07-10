68df80478b0d7abb658544f6af8d9900cd6c659f
anishgondhi04 <anish.gondhi@dal.ca>
2026-07-10 09:59:35 -0300

    feat: bootstrap http server with health route, closes #1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	log.Println("go-distributed-scheduler: bootstrap server coming next")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"status":"ok"}`))
+	})
+
+	fs := http.FileServer(http.Dir("./web"))
+	mux.Handle("/", fs)
+
+	addr := ":8080"
+	log.Printf("go-distributed-scheduler listening on %s", addr)
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatal(err)
+	}
 }
